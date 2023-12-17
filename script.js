7f9e3fb1560288c95b6b74e55ae190d952b6c5db
Chris <148640978+ChrisUK3069@users.noreply.github.com>
2023-12-17 22:52:13 +0000

    Added basic UI, sans style

--- script.js
+++ script.js
@@ -42,30 +42,75 @@ function game(){
     }
 
     
-let playerSelection;
+let playerSelection; 
 let computerSelection = getComputerChoice();    // Generates a random number 1-3 and assigns to a string value
 let userScore = 0;
 let computerScore = 0;
 
-function begin() {
+function begin(playerSelection) {
     console.log(" ** Welcome to Rock Paper Scissors! **  ");
+    const results = document.querySelector("#results");
+    const scores = document.querySelector("#scores");
+    const winner = document.querySelector("#winner");
     
-    for (let step = 1; step < 6; step++) {
-        playerSelection = prompt("Please enter rock, paper or scissors: ");
-        playerSelection = playerSelection.toLowerCase();
+    //for (let step = 1; step < 6; step++) { remove logic to play 5 rounds
+        //playerSelection = prompt("Please enter rock, paper or scissors: ");
+        //playerSelection = playerSelection.toLowerCase();
 
         computerSelection = getComputerChoice();
-        console.log("Round:" + step, game());
+        console.log("Round:" , game()); // + step CODE FOR LOOP
         console.log(" - User: " + userScore + ", Computer: " + computerScore)
-    }
+    //}
 
     if (userScore > computerScore) {
-            console.log("Congratulations, you won the game!");
+            console.log("You win, earnt 1 point.");
+            results.textContent = `You chose ` + playerSelection + 
+            `.  Opponent chose ` + computerSelection + 
+            `.  You win, earnt 1 point.`;
         } else if (computerScore > userScore) {
-            console.log("You lost the game, please try again.")
+            console.log("You lost, please try again.")
+            results.textContent = `You chose ` + playerSelection + 
+            `.  Opponent chose ` + computerSelection + 
+            `.  You lost, try again.`;
         } else {
             console.log("Scores are tied, please try again.")
-        }
+            results.textContent = `You chose ` + playerSelection + 
+            `.  Opponent chose ` + computerSelection + 
+            `.  It's a tie, try again.`;
+         }
+
+    scores.textContent = `Running score, your points : ` + userScore + `
+                        Opponents points ` + computerScore;
+
+    if (userScore === 5) {
+         winner.textContent = "5 Points earned, you have won the game!"
+    } else if (computerScore === 5) {
+         winner.textContent = "Opponent reaches 5 points, Player Loses."
+    }
+                        
+
+
+
 }
 
-begin();
+
+
+
+
+const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", function ()  {
+    playerSelection = "rock";
+    begin(playerSelection);
+});
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", function ()  {
+    playerSelection = "paper";
+    begin(playerSelection);
+});
+
+const btnScissors = document.querySelector("#btnScissors");
+btnScissors.addEventListener("click", function () {
+    playerSelection = "scissors";
+    begin(playerSelection);
+});
